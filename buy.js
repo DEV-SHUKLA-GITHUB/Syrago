d24b5f57a8734864e25c5a3eba8fe91c7c83ab2d
Dev Shukla <devshuklaji6@gmail.com>
2023-01-01 23:25:32 +0530

    added some things to the website

--- buy.js
+++ buy.js
@@ -1,7 +1,22 @@
-fetch("https://fakestoreapi.com/products/1")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((user) => {
-      const elements = ``;
+fetch("https://fakestoreapi.com/products/")
+  .then((data) => data.json())
+  .then((completedata) => {
+    // console.log(data);
+    let elements = "";
+    completedata.map((item) => {
+      elements += `
+        <div class="card">
+            <img src="${item.image}" class="image" alt="">
+            <p class="place">${item.category}</p>
+            <p class="para">${item.price}</p>
+        </div>`;
     });
+    document.getElementById("main-div").innerHTML = elements;
+    function handleClick() {
+      console.log("clicked");
+    }
+  })
+  .catch((e) => {
+    console.log(e);
   });
+// });
